cc554bba14e76f4035ef3b00196b3750d96eefa5
Vithurshini Subramanieam <vsupu26@gmail.com>
2023-03-19 00:43:47 +0530

    Updates

---
+++ precache-manifest.0f59dc2ffe26401f14380705b02bd990.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a2524ddbf409444d7e",
+    "url": "/CalistusTrophy/static/css/main.8420a3c7.chunk.css"
+  },
+  {
+    "revision": "a0a2524ddbf409444d7e",
+    "url": "/CalistusTrophy/static/js/main.a0a2524d.chunk.js"
+  },
+  {
+    "revision": "203309d7e863bf89b9eb",
+    "url": "/CalistusTrophy/static/js/1.203309d7.chunk.js"
+  },
+  {
+    "revision": "0cc6c6440e43d07d643d",
+    "url": "/CalistusTrophy/static/css/2.3341c2d8.chunk.css"
+  },
+  {
+    "revision": "0cc6c6440e43d07d643d",
+    "url": "/CalistusTrophy/static/js/2.0cc6c644.chunk.js"
+  },
+  {
+    "revision": "eb2c81303482c031e87b",
+    "url": "/CalistusTrophy/static/js/runtime~main.eb2c8130.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CalistusTrophy/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CalistusTrophy/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CalistusTrophy/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CalistusTrophy/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2be9c0da81f7f9633a2c353d2085c40e",
+    "url": "/CalistusTrophy/static/media/CalistusVideo.2be9c0da.mp4"
+  },
+  {
+    "revision": "d28e7bbc4bc9f1ebd3ddc9729269d71b",
+    "url": "/CalistusTrophy/static/media/CalistusBrother.d28e7bbc.jpeg"
+  },
+  {
+    "revision": "1946cf5037a4e7619a14688ec94d7f44",
+    "url": "/CalistusTrophy/static/media/selfie.1946cf50.jpeg"
+  },
+  {
+    "revision": "cdb580989c6d9627f9529aa91530f64b",
+    "url": "/CalistusTrophy/static/media/4teams.cdb58098.jpeg"
+  },
+  {
+    "revision": "d0426e583bf446a06ec58e347ab23660",
+    "url": "/CalistusTrophy/static/media/team16a.d0426e58.jpeg"
+  },
+  {
+    "revision": "0bdd355b2391d1bdfc050cb0265a2cfa",
+    "url": "/CalistusTrophy/static/media/team17a.0bdd355b.jpeg"
+  },
+  {
+    "revision": "51d4785227e59e12fcdce4d06a764c95",
+    "url": "/CalistusTrophy/static/media/team18a.51d47852.jpeg"
+  },
+  {
+    "revision": "6024f791bc46ba2df052c88cd9948f1b",
+    "url": "/CalistusTrophy/static/media/team19a.6024f791.jpeg"
+  },
+  {
+    "revision": "0f3b64b7e58f6897e743bf0331b8a287",
+    "url": "/CalistusTrophy/static/media/calistusCoin.0f3b64b7.png"
+  },
+  {
+    "revision": "681dfb4b212db9c4e28c219b89b12aab",
+    "url": "/CalistusTrophy/static/media/CoinHandOver.681dfb4b.jpeg"
+  },
+  {
+    "revision": "6aab363fafd781424332a9aba11478fd",
+    "url": "/CalistusTrophy/static/media/groundView.6aab363f.jpg"
+  },
+  {
+    "revision": "8660a5d2c0bdca41c76e816ff6dfbd4a",
+    "url": "/CalistusTrophy/static/media/defendingCaptain.8660a5d2.jpeg"
+  },
+  {
+    "revision": "4eeb480f991a5d8f2bcd3883aacd7d77",
+    "url": "/CalistusTrophy/static/media/bm1.4eeb480f.jpeg"
+  },
+  {
+    "revision": "4163a1b1e97a24ae22b74883078ba87e",
+    "url": "/CalistusTrophy/static/media/bm2.4163a1b1.jpeg"
+  },
+  {
+    "revision": "cdca4e257a1c127ffe47de10ca4eb43d",
+    "url": "/CalistusTrophy/static/media/bm3.cdca4e25.jpeg"
+  },
+  {
+    "revision": "48c077578e5b56fb5a0415de2882bb4c",
+    "url": "/CalistusTrophy/static/media/bm4.48c07757.jpeg"
+  },
+  {
+    "revision": "84ca9a4f3349ae76b3c630d724990ded",
+    "url": "/CalistusTrophy/static/media/bm5.84ca9a4f.jpeg"
+  },
+  {
+    "revision": "881b3738e6ade6112636d4f9f0f4be3a",
+    "url": "/CalistusTrophy/static/media/bm6.881b3738.jpeg"
+  },
+  {
+    "revision": "62190d768ce029e736a4fb9e7c2d4396",
+    "url": "/CalistusTrophy/static/media/bm7.62190d76.jpeg"
+  },
+  {
+    "revision": "978186c4baa5ee96fad358ad95c9ae6a",
+    "url": "/CalistusTrophy/static/media/bm8.978186c4.jpeg"
+  },
+  {
+    "revision": "3beb4bd614618b0a4bf3e0069ce40269",
+    "url": "/CalistusTrophy/static/media/NavaSir1.3beb4bd6.jpeg"
+  },
+  {
+    "revision": "2cefa869aacb223083872493317c84b6",
+    "url": "/CalistusTrophy/static/media/NavaSir2.2cefa869.jpeg"
+  },
+  {
+    "revision": "1a9bcc79dd058ccadb78de7cb2ae94a8",
+    "url": "/CalistusTrophy/static/media/NavaSir3.1a9bcc79.jpeg"
+  },
+  {
+    "revision": "280c73fe0fd5b797d19c3bd15ef52f80",
+    "url": "/CalistusTrophy/static/media/NavaSir4.280c73fe.jpeg"
+  },
+  {
+    "revision": "cad3969afeaa48a9dde7bd565d44b18e",
+    "url": "/CalistusTrophy/static/media/q11.cad3969a.jpeg"
+  },
+  {
+    "revision": "ed1610cd3dfed1f910071fc0b1bf6340",
+    "url": "/CalistusTrophy/static/media/q12.ed1610cd.jpeg"
+  },
+  {
+    "revision": "f6fb22faf14f6fda94d9dd51f7193c87",
+    "url": "/CalistusTrophy/static/media/q13.f6fb22fa.jpeg"
+  },
+  {
+    "revision": "231fa1334ee7739e87d9727580863cbf",
+    "url": "/CalistusTrophy/static/media/e1.231fa133.jpg"
+  },
+  {
+    "revision": "60191b59a0496e43a5ddf6ab68d973fd",
+    "url": "/CalistusTrophy/static/media/e2.60191b59.jpg"
+  },
+  {
+    "revision": "5c591c1c2af202ad6206c661d00da58b",
+    "url": "/CalistusTrophy/static/media/q21.5c591c1c.jpg"
+  },
+  {
+    "revision": "b57705e6f82969799f151e421f4e7e33",
+    "url": "/CalistusTrophy/static/media/q22.b57705e6.jpg"
+  },
+  {
+    "revision": "14df4d19506cc1b21cd290e9c4864cfc",
+    "url": "/CalistusTrophy/static/media/q23.14df4d19.jpg"
+  },
+  {
+    "revision": "e66e847cf38e21753fc0be28e397ed43",
+    "url": "/CalistusTrophy/static/media/ft1.e66e847c.jpeg"
+  },
+  {
+    "revision": "2d146e5912b9f9e6d2031432a5f0da1c",
+    "url": "/CalistusTrophy/static/media/ft2.2d146e59.jpeg"
+  },
+  {
+    "revision": "adcd22f3c086d8489e15a6bb0ac59835",
+    "url": "/CalistusTrophy/static/media/nmatch1.adcd22f3.jpeg"
+  },
+  {
+    "revision": "b237f2d0381cf01a27622ffa9e75c6b8",
+    "url": "/CalistusTrophy/static/media/nmatch2.b237f2d0.jpeg"
+  },
+  {
+    "revision": "5b361509078b593ed83a0b302a77732c",
+    "url": "/CalistusTrophy/static/media/nmatch3.5b361509.jpeg"
+  },
+  {
+    "revision": "71a979ca226fb9493da429ec364338fa",
+    "url": "/CalistusTrophy/static/media/dmatch1.71a979ca.jpeg"
+  },
+  {
+    "revision": "a05c48179cce77dc167879f1b612e0d2",
+    "url": "/CalistusTrophy/static/media/dmatch2.a05c4817.jpeg"
+  },
+  {
+    "revision": "14c91f949c161f3b8f4613b604d07515",
+    "url": "/CalistusTrophy/static/media/dmatch3.14c91f94.jpeg"
+  },
+  {
+    "revision": "1e4889170d40deabafee43df3046c090",
+    "url": "/CalistusTrophy/static/media/dmatch4.1e488917.jpeg"
+  },
+  {
+    "revision": "cfa43c59a74acb86fb698c67fed82892",
+    "url": "/CalistusTrophy/static/media/thrill.cfa43c59.jpeg"
+  },
+  {
+    "revision": "c74f09ba303db9a3db8ab80958a29b86",
+    "url": "/CalistusTrophy/static/media/dis1.c74f09ba.jpeg"
+  },
+  {
+    "revision": "a4ec919abf6f61c1da784e7bd4c9117f",
+    "url": "/CalistusTrophy/static/media/dis2.a4ec919a.jpeg"
+  },
+  {
+    "revision": "eede609c083f6df0e1f64e4eb6a33c7e",
+    "url": "/CalistusTrophy/static/media/victory1.eede609c.jpeg"
+  },
+  {
+    "revision": "3a007a195f8ad090767384a5c8deedaa",
+    "url": "/CalistusTrophy/static/media/victory2.3a007a19.jpeg"
+  },
+  {
+    "revision": "b7b453808f42cfc13de0caccf0475ae6",
+    "url": "/CalistusTrophy/static/media/victory3.b7b45380.jpeg"
+  },
+  {
+    "revision": "fef2e4eaeb5be01e4728d1807cb8d8b9",
+    "url": "/CalistusTrophy/static/media/victory4.fef2e4ea.jpeg"
+  },
+  {
+    "revision": "d830081ee574946139f50bf386e48e8d",
+    "url": "/CalistusTrophy/static/media/victory5.d830081e.jpeg"
+  },
+  {
+    "revision": "8aa63484a83c9bb56f6d415e2fcc3376",
+    "url": "/CalistusTrophy/static/media/victory6.8aa63484.jpeg"
+  },
+  {
+    "revision": "f631514ed34abc0ce3a101f11df0c6a0",
+    "url": "/CalistusTrophy/static/media/victory7.f631514e.jpeg"
+  },
+  {
+    "revision": "4dbbce25dce35fe8753c8e5eb9b3c1db",
+    "url": "/CalistusTrophy/static/media/sports1.4dbbce25.jpeg"
+  },
+  {
+    "revision": "0493385d65c0d6b893bbad390ee987a8",
+    "url": "/CalistusTrophy/static/media/sports2.0493385d.jpeg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/CalistusTrophy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8a0ddb808726c7ac875fb64436dcce3d",
+    "url": "/CalistusTrophy/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CalistusTrophy/precache-manifest.0f59dc2ffe26401f14380705b02bd990.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CalistusTrophy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
