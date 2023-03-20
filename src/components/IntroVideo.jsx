b9e2a7e7a491c51a9a5f00761c482927c582c1a2
Vithurshini Subramanieam <vsupu26@gmail.com>
2023-03-20 19:06:31 +0530

    new change

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -4,9 +4,9 @@ import CalistusVideo from '../assets/CalistusVideo.mp4'
 const IntroVideo = () =>{
     return(
         <div className='introvideo' style={{overflow:'hidden'}}>
-            
-            <video src= {CalistusVideo} autoPlay loop muted></video>
             <div className="overlay"></div>
+            <video src= {CalistusVideo} autoPlay loop muted></video>
+            
             <div className="content">
                 <h1 className='titletext'>Calistus Trophy</h1>
                 <p>A decade down, forever to go!</p>
